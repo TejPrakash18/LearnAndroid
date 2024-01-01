05f2d65835d0e592c5d28db74c1f939556598f87
TejPrakash18 <tej22upa.dhyay@gmail.com>
2024-01-01 19:57:32 +0530

    Scaffold

--- Scaffold/app/src/main/java/com/tejdev/scaffold/MainActivity.kt
+++ Scaffold/app/src/main/java/com/tejdev/scaffold/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.tejdev.scaffold
 
 import android.os.Bundle
+import android.provider.CalendarContract.Colors
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -16,36 +23,24 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.tejdev.scaffold.screens.SmallTopAppBarExample
 import com.tejdev.scaffold.ui.theme.ScaffoldTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            SmallTopAppBarExample()
         }
     }
 }
 
-@Preview
-@Composable
-fun AppToolBarWithBottonBar() {
-    val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
-    Scaffold(
-        scaffoldState = scaffoldState,
-        topBar = { TopAppBar(title = {Text("Top App Bar")},backgroundColor = MaterialTheme.colors.primary)  },
-        floatingActionButtonPosition = FabPosition.End,
-        floatingActionButton = { FloatingActionButton(onClick = {}){
-            Icon(imageVector = Icons.Default.Add, contentDescription = "fab icon")
-        } },
-        drawerContent = { Text(text = "Drawer Menu 1") },
-        content = { Text("Content") },
-        bottomBar = { BottomAppBar(backgroundColor = MaterialTheme.colors.primary) { Text("Bottom App Bar") } }
-    )
 
-}
 
---
+++ Scaffold/app/src/main/java/com/tejdev/scaffold/screens/AppTopBar.kt
@@ -0,0 +1,66 @@
+package com.tejdev.scaffold.screens
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showSystemUi = true)
+@Composable
+fun SmallTopAppBarExample() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,),
+                title = {
+                    Text("Small Top App Bar")
+                }
+            )
+        },
+    ){ innerPadding ->
+        ScrollContent(innerPadding)
+    }
+}
+
+@Composable
+fun CardList() {
+    Card(elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+        border = BorderStroke(4.dp, Color.Blue),
+        modifier = Modifier.padding(10.dp)
+    ) {
+
+    }
+}
+@Composable
+fun ScrollContent(innerPadding: PaddingValues) {
+    val range = 1..100
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentPadding = innerPadding,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(range.count()) { index ->
+            Text(text = "- List item number ${index + 1}")
+        }
+    }
+}
